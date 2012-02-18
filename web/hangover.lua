d976c3047bbebc59abab317cb86c7ed3e4f5fcfb
Kacper Wysocki <comotion@users.sf.net>
2012-02-19 00:06:16 +0100

    dont know

---
+++ web/hangover.lua
@@ -0,0 +1,35 @@
+require "orbit"
+
+module("hangover", package.seeall, orbit.new)
+require "db"
+
+function render(content)
+  return html {
+    head { title("Hangover"),
+           meta{ ["http-equiv"] = "Content-Type",
+             content = "text/html; charset=utf-8" },
+           link{ rel = 'stylesheet', type = 'text/css', 
+             href = '/p/style.css', media = 'screen'}
+  },
+    body { 
+      div{ id="content",
+        content 
+      }
+    }
+  }
+end
+
+
+function index()
+  return render(
+  )
+end
+
+hangover:dispatch_static("/p/.+")
+hangover:dispatch_get(index, "/")
+
+hangover:htmlify("render")
+
+
+return _M
+
---
+++ web/model.lua
@@ -0,0 +1,39 @@
+
+-- MODEL ------------------------
+--
+-- objects:
+--   - track
+--     - length
+--     - artist
+--     - path
+--     - id
+--     - mood
+--     - ...
+--   - selector
+--     - station
+--     - name
+--     - criteria
+--       - mood
+--         - happy
+--   - program
+--     - station
+--     - recourring -> weekly, daily, blank
+--     - start
+--     - stop
+--     - selector
+--     - items
+--   
+--   - stations
+--     - name
+--     - selector
+---------------------------------
+
+-- require "tokyocabinet"
+
+module("db", package.seeall)
+
+
+
+
+
+
