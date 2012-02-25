393cb1ebf987cbc387ff7f8c8798f30fae3baa03
Kacper Wysocki <comotion@krutt.org>
2012-02-25 21:11:51 +0100

    web methods like dat

--- web/hangover.lua
+++ web/hangover.lua
@@ -1,14 +1,15 @@
-require "orbit"
+local orbit = require "orbit"
+local inspect =require "inspect"
 
 module("hangover", package.seeall, orbit.new)
 require "lib/model"
 
-function render(content)
+function render(t, content)
   return html {
-    head { title("Hangover"),
+    head { title("Hangover - " .. t),
            meta{ ["http-equiv"] = "Content-Type",
              content = "text/html; charset=utf-8" },
-           link{ rel = 'stylesheet', type = 'text/css', 
+           link{ rel = 'stylesheet', type = 'text/css',  
              href = '/p/style.css', media = 'screen'}
   },
     body { 
@@ -21,14 +22,35 @@ end
 
 
 function index()
-  return render(
-  )
+  return render("hello","world")
 end
 
-hangover:dispatch_static("/p/.+")
-hangover:dispatch_get(index, "/")
+function render_add_track()
+  return render("Add track", html{
+     "something something"
+  })
+end
+
+function render_add_track2()
+  return orbit.web_methods.page("views/add_track")
+end
+
+function view_web(web)
+  print(inspect( web)) -- debug a little.
+  return render("web!",html { li{a{ href= web:link("/"), "HOME" }} })
+end
 
 hangover:htmlify("render")
+hangover:htmlify("render_add_track")
+hangover:htmlify("view_web")
+
+
+hangover:dispatch_get(index, "/")
+hangover:dispatch_get(render_add_track, "/track")
+hangover:dispatch_get(render_add_track2, "/wtf")
+hangover:dispatch_get(view_web, "/stfu")
+
+hangover:dispatch_static("/p/.+")
 
 return _M
 
