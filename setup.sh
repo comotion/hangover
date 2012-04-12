e774c41ca64501cbc7b98b314e15e3004c11bff5
root <root@bt.(none)>
2012-04-12 15:40:49 -0400

    makeitwork script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
-git clone git://github.com/comotion/hangover
+#git clone git://github.com/comotion/hangover
 apt-get install luarocks zlib1g-dev
 luarocks install orbit
 
-wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -O tokyo.tgz
-tar xvf tokyo.tgz
-wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+# somethings wrong with the debian headers
+#apt-get install tokyocabinet-bin libtokyocabinet-dev
+wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
+cd tokyocabinet-1.4.47 && ./configure && make && make install && cd -
 
-wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -O - | tar xvf -
-cd tokyo*
-./configure
-make
-make install
+wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -qO - | tar xzf -
+cd bzip2-* && make && make install
+
+wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -qO - | tar xzf -
+cd tokyocabinet-lua*
+./configure && make && make install && cd -
+cd web
+[ ! -d sabot ] && git clone git://github.com/comotion/sabot || ( cd sabot && git pull )
