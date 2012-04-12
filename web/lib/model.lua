0f9bba7b96d85c1e4df32bc03ba27217c75b7164
root <root@bt.(none)>
2012-04-12 15:41:58 -0400

    model search less idiot

--- web/lib/model.lua
+++ web/lib/model.lua
@@ -1,34 +1,18 @@
-
 -- MODEL ------------------------
---
 -- objects:
 --   - track
---     - length
---     - artist
---     - path
---     - id
---     - mood
---     - ...
+--     - length - artist - path - id - mood
 --   - selector
---     - station
---     - name
---     - criteria
---       - mood
---         - happy
+--     - station - name - criteria - mood - happy
 --   - program
---     - station
---     - recourring -> weekly, daily, blank
---     - start
---     - stop
---     - selector
---     - items
---   
+--     - station - start - stop - selector - items
+--     - recurring -> weekly, daily, blank
 --   - stations
---     - name
---     - selector
+--     - name - selector
 ---------------------------------
 
 require "tokyocabinet"
+oh = require "sabot/botlib"
 
 module("tracks", package.seeall)
 
@@ -42,46 +26,77 @@ end
 
 tracks:init() 
 
-function tracks:add()
+function tracks:add(art, ttl)
   pkey = trk:genuid()
-  cols = { artist = "mordi", title = "dance music" }
+  cols = { artist = art, title = ttl }
   if not trk:put(pkey, cols) then
     ecode = trk:ecode()
     print("Error adding track: " .. trk:errmsg(ecode))
   else
-    print("Added track:" .. pkey)
-  end
+    print("Added track:" .. pkey .. ": " .. art.. "-" .. ttl )
+    end
 end
+
+-- fix this stupidity. if you spell the query type wrong, you get nothing, no warnings either.
+local q = tokyocabinet.tdbqrynew(trk)
+local op = {
+  -- string
+  equal     = q.TDBQCSTREQ,
+  inclusive = q.TDBQCSTRSTRINC,
+  begins    = q.TDBQCSTRBW,
+  ends      = q.TDBQCSTREW,
+  all       = q.TDBQCSTRAND,
+  one       = q.TDBQCSTROR,
+  eqone     = q.TDBQCSTROREQ,
+  -- numeric
+  eq        = q.QCNUMEQ,
+  gt        = q.QCNUMGT,
+  ge        = q.QCNUMGE,
+  lt        = q.QCNUMLT,
+  le        = q.QCNUMLE,
+  tween     = q.QCNUMBT,
+  numor     = q.QCNUMOREQ,
+  -- fulltext
+  phrase    = q.QCFTSPH,
+  alltokens = q.QCFTSAND,
+  onetoken  = q.QCFTSOR,
+  compound  = q.QCFTSEX,
+  -- flags
+  negate    = q.QCNEGATE,
+  noindex   = q.QCNOIDX,
+}
 
 function tracks:searchtest()
   q = tokyocabinet.tdbqrynew(trk)
-  --`op' specifies an operation type: 
-  --`TDBQCSTREQ' for string which is equal to the expression, 
-  --`TDBQCSTRINC' for string which is included in the expression, 
-  --`TDBQCSTRBW' for string which begins with the expression, 
-  --`TDBQCSTREW' for string which ends with the expression, 
-  --`TDBQCSTRAND' for string which includes all tokens in the expression, 
-  --`TDBQCSTROR' for string which includes at least one token in the expression, 
-  --`TDBQCSTROREQ' for string which is equal to at least one token in the expression, 
-  --`TDBQCSTRRX' for string which matches regular expressions of the expression, 
-  --`TDBQCNUMEQ' for number which is equal to the expression, 
-  --`TDBQCNUMGT' for number which is greater than the expression, 
-  --`TDBQCNUMGE' for number which is greater than or equal to the expression, 
-  --`TDBQCNUMLT' for number which is less than the expression, 
-  --`TDBQCNUMLE' for number which is less than or equal to the expression, 
-  --`TDBQCNUMBT' for number which is between two tokens of the expression, 
-  --`TDBQCNUMOREQ' for number which is equal to at least one token in the expression, 
-  --`TDBQCFTSPH' for full-text search with the phrase of the expression, 
-  --`TDBQCFTSAND' for full-text search with all tokens in the expression, 
-  --`TDBQCFTSOR' for full-text search with at least one token in the expression, 
-  --`TDBQCFTSEX' for full-text search with the compound expression. 
-  -- All operations can be flagged by bitwise-or: `TDBQCNEGATE' for negation, `TDBQCNOIDX' for using no index.
 
-  q:addcond("artist", q.QSCTRINC, "mordi")
+  q:addcond("artist", op.equal, "mordi")
   res = q:search()
-  for i = 1, #res do
-    c = trk:get(res[i])
+  c = "tracks: "
+  print (c, oh.dump(res))
+  --for i = 1, #res do
+    --c = c .. "<br>" .. trk:get(res[i])
+  --end
+  --return c
+end
+
+function tracks:search(query)
+  q = tokyocabinet.tdbqrynew(trk)
+  q:addcond("artist", op.phrase, query)
+  res = q:search()
+  return oh.dump(res)
+end
+
+function tracks:dump()
+  trk:iterinit()
+  local key, value, accu
+  accu = {}
+  while true do
+    key = trk:iternext()
+    if not key then break end
+    value = trk:get(key)
+    table.insert(accu,value)
   end
+  return oh.dump(accu)
 end
 
 function tracks:get(id)
@@ -96,6 +111,12 @@ function tracks:update(id, cols)
   trk:put(id, cols)
 end
 
-print(tracks:searchtest())
+--print("TEST SEARCH:")
+--print(tracks:searchtest())
+--print(tracks:search("mordi"))
+--print(tracks:search("world"))
+print("TRACK DUMP:")
+print(tracks:dump())
 
 
+return tracks
