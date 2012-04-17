18a981fa9b0c0f557ec1eafd4d055e7cdb134756
root <root@bt.(none)>
2012-04-17 13:52:26 -0400

    indempotence and not b0rk

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #git clone git://github.com/comotion/hangover
 apt-get install luarocks zlib1g-dev
-luarocks install orbit lua-cjson
+luarocks install orbit
+luarocks install lua-cjson
 
 # somethings wrong with the debian headers
 #apt-get install tokyocabinet-bin libtokyocabinet-dev
-wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
+[ -d tokyocabinet-1.4.47 ] || wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
 cd tokyocabinet-1.4.47 && ./configure && make && make install && cd -
 
-wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -qO - | tar xzf -
+[ -d bzip2-1.0.6 ] || wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -qO - | tar xzf -
 cd bzip2-* && make && make install
 
-wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -qO - | tar xzf -
+[ -d tokyocabinet-lua-1.10 ] || wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -qO - | tar xzf -
 cd tokyocabinet-lua*
 ./configure && make && make install && cd -
 cd web
