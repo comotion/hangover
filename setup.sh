92be9403fbc77ee1a570e93058916e2133e3a81a
Kacper Wysocki <comotion@users.sf.net>
2012-04-23 20:18:30 +0200

    setup should install bz2 before tc

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ luarocks install lua-cjson
 
 # somethings wrong with the debian headers
 #apt-get install tokyocabinet-bin libtokyocabinet-dev
-[ -d tokyocabinet-1.4.47 ] || wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
-cd tokyocabinet-1.4.47 && ./configure && make && make install && cd -
-
 [ -d bzip2-1.0.6 ] || wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -qO - | tar xzf -
 cd bzip2-* && make && make install
+
+[ -d tokyocabinet-1.4.47 ] || wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
+cd tokyocabinet-1.4.47 && ./configure && make && make install && cd -
 
 [ -d tokyocabinet-lua-1.10 ] || wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -qO - | tar xzf -
 cd tokyocabinet-lua*
