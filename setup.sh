ee701a620e353d08103bea1952e5b9f31ac485fe
root <root@bt.(none)>
2012-05-08 01:58:01 +0200

    set it up gddmit

--- setup.sh
+++ setup.sh
@@ -6,13 +6,18 @@ luarocks install lua-cjson
 # somethings wrong with the debian headers
 #apt-get install tokyocabinet-bin libtokyocabinet-dev
 [ -d bzip2-1.0.6 ] || wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -qO - | tar xzf -
-cd bzip2-* && make && make install
+cd bzip2-* 
+make && make install
+cd -
 
 [ -d tokyocabinet-1.4.47 ] || wget http://fallabs.com/tokyocabinet/tokyocabinet-1.4.47.tar.gz -qO - | tar xzf -
-cd tokyocabinet-1.4.47 && ./configure && make && make install && cd -
+cd tokyocabinet-1.4.47 && ./configure && make ;make install
+cd -
+
 
 [ -d tokyocabinet-lua-1.10 ] || wget http://fallabs.com/tokyocabinet/luapkg/tokyocabinet-lua-1.10.tar.gz -qO - | tar xzf -
 cd tokyocabinet-lua*
-./configure && make && make install && cd -
+./configure && make && make install
+cd -
 cd web
 #[ ! -d sabot ] && git clone git://github.com/comotion/sabot || ( cd sabot && git pull )
