572a203d670546619d0e5d835dad1870178b1529
why <kwy@localhost.localdomain>
2012-10-02 03:50:05 +0200

    Initial playlist commit.

---
+++ web/lib/playlist.lua
@@ -0,0 +1,14 @@
+-- playlist
+--- stores a list of tracks in order
+local db  = require "lib.tokyo"
+local u   = require "lib.util"
+
+module(..., package.seeall)
+
+function playlist:init()
+  return tokyo:init("playlists")
+end
+pls = tokyo:init()
+
+function playlist:put(pkey,cols)
+
